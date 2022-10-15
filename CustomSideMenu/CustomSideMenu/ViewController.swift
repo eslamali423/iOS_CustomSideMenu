4a439906f3a9df5fd0c5aa74481364d1b92fd0af
eslamali <eslam.ali4233@gmail.com>
2022-10-15 14:15:51 +0200

    init xcode project commit

---
+++ CustomSideMenu/CustomSideMenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomSideMenu
+//
+//  Created by Eslam Ali  on 15/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
